bfb887c6f57dfe38065245df823c036b56a3c1f4
PabloRuizDiaz <Pablo32784964>
2020-08-31 20:37:59 -0300

    Revert "Merge branch 'master' of https://github.com/InoveAlumnos/archivos_python"
    This reverts commit 3bdd92168970b1a883040206718a8c48f285923f, reversing
    changes made to e53acb86efb2192fb9ab64d52a53390b976c0e39.

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -4,7 +4,7 @@ Archivos [Python]
 Ejercicios de práctica
 ---------------------------
 Autor: Inove Coding School
-Version: 1.3
+Version: 1.2
 
 Descripcion:
 Programa creado para que practiquen los conocimietos
