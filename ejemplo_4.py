eeb8969a6488572df876f5300c9b55d689cc52ed
inoveAlumnos <alumnos@inove.com.ar>
2023-03-18 17:55:41 -0300

    Ejemplo 4

---
+++ ejemplo_4.py
@@ -0,0 +1,18 @@
+# Archivos [Python]
+# Ejemplos de clase
+
+# Autor: Inove Coding School
+# Version: 2.2
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    print('''
+    Profe:
+    Deberá subir el archivo ejemplo_3.py modificado
+    al repositorio de archivos. Para eso:
+    - Forkear el repositorio de clase "archivos"
+    - Utilizar la interfaz web y "Add file"
+      suba el archivo ejemplo_3.py
+    - Verificar en github que su archivo ha sido modificado
+    ''')
