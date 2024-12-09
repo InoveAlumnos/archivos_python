417c8c224a775bb051c172462edad2faa8e49af3
inoveAlumnos <alumnos@inove.com.ar>
2024-12-08 21:15:36 -0300

    Ejercicios practica

---
+++ ejercicios_practica/ejercicio_1.py
@@ -0,0 +1,28 @@
+
+# CODE:38
+# IMPORTANTE: NO borrar los comentarios
+
+import csv
+
+
+def desafio():
+    print('Ejercicio de archivos')
+    archivo = 'stock.csv'
+    
+    # Realice un programa que abra el archivo 'stock.csv'
+    # en modo lectura y cuente el stock total de tornillos
+    # a lo largo de todo el archivo, 
+    # sumando el stock en cada fila del archivo
+
+    # Para eso debe leer los datos del archivo
+    # con "csv.DictReader", y luego recorrer los datos
+    # dentro de un bucle y solo acceder a la columna "tornillos"
+    # para cumplir con el enunciado del ejercicio
+
+    # Al final de esta función retornar (return) el stock total de tornillos
+    # Comenzar aquí, recuerde el identado dentro de esta funcion
+    
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    desafio()
